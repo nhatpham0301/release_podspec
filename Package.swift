2c7b4ff9f1c4509b394f73bdb964fc27bc2f0d2d
nhatpv <nhatpv@vietmap.vn>
2023-07-07 10:31:55 +0700

    Change config

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VietMap",
-            url: "https://github.com/nhatpham0301/release_podspec/blob/main/VietMap.xcframework.zip",
+            url: "https://github.com/nhatpham0301/release_podspec/raw/main/VietMap.xcframework.zip",
             checksum: "902e68d2bb6f53ac3e545579f587e7542e06703076f4851abe89e7ba4d6b93f7") 
             ]
 )
